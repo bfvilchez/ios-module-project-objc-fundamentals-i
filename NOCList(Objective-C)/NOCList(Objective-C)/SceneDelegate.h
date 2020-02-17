2054ce524c9afa484f31b10ba6738737b569afff
brian <bfvilchez95@gmail.com>
2020-02-17 16:55:26 -0700

    created Xcode Project

---
+++ NOCList(Objective-C)/NOCList(Objective-C)/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  NOCList(Objective-C)
+//
+//  Created by brian vilchez on 2/17/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
